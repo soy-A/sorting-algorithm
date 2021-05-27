96dbc7840deb9eecf91c2cd7b2ce48c8050d7cbe
soy-A <skql1502@gmail.com>
2021-05-28 04:59:00 +0900

    Add button and algorithms

--- docs/graph.js
+++ docs/graph.js
@@ -1,7 +1,7 @@
 // 랜덤하게 섞인 배열 생성
 var data = [];
 for(let i = 0; i < 100; i++){
-  data[i] = i + 1;
+		data[i] = i + 1;
 }
 
 function initRandomArray(array){
@@ -28,13 +28,13 @@ var y = d3.scaleLinear()
     .range([height - margin.bottom, margin.top]);
     
 var xAxis = g => g
-		.attr('transform', 'translate(0, ${height - margin.bottom})')
+/* 		.attr('transform', 'translate(0, ${height - margin.bottom})') */
     .call(d3.axisBottom(x)
     	.tickSizeOuter(0));
 
 var svg = d3.select('body').append('svg').style('width', width).style('height', height);
 
-svg.append('g').call(xAxis);
+/* svg.append('g').call(xAxis); */
 svg.append('g')
 		.selectAll('rect').data(data).enter().append('rect')
     .attr('x', d => x(d))
@@ -45,3 +45,164 @@ svg.append('g')
     .attr('fill', '#1E3269');
 
 svg.node();
+
+// 딜레이를 위한 타이머
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+// 선택 정렬
+async function selectionSort(data) {
+		for(let i = 0; i < data.length; i++) {
+				let minIndex = i;
+				for(let j = i + 1; j < data.length; j++) {
+      			if(data[minIndex] > data[j]) {
+        				minIndex = j;
+       			 }
+    		}
+     		if(minIndex !== i) {
+      			let temp = data[minIndex];
+        		data[minIndex] = data[i];
+        		data[i] = temp;
+      	}
+      	await timer(100);
+        
+				svg.selectAll('rect').data(data)
+          	.attr('y', d => y(d))
+          	.attr('height', d => y(0) - y(d));
+      	svg.node();
+    }
+}
+
+// 버블 정렬
+async function bubbleSort(data) {
+		for(let i = 0; i < data.length; i++) {
+				let temp;
+				for(let j = 0; j < data.length - 1 - i; j++) {
+						if(data[j] > data[j + 1]) {
+								temp = data[j];
+								data[j] = data[j + 1];
+								data[j + 1] = temp;
+						}
+				}
+        await timer(100);
+        
+				svg.selectAll('rect').data(data)
+        		.attr('y', d => y(d))
+            .attr('height', d => y(0) - y(d));
+				svg.node();
+		}
+}
+
+// 삽입 정렬
+async function insertionSort(data) {
+		for(let i = 1; i < data.length; i++) {
+				let cur = data[i];
+				let left = i - 1;
+				while(left >= 0 && data[left] > cur) {
+						data[left + 1] = data[left];
+						data[left] = cur;
+						cur = data[left];
+						left--;
+				}
+        await timer(100);
+        
+        svg.selectAll('rect').data(data)
+        		.attr('y', d => y(d))
+            .attr('height', d => y(0) - y(d));
+				svg.node();
+		}
+}
+
+// 병합 정렬
+async function merge (left, right) {
+		let resultArray = [];
+		let leftIndex = 0;
+		let rightIndex = 0;
+    
+		while (leftIndex < left.length && rightIndex < right.length) {
+				if (left[leftIndex] < right[rightIndex]) {
+						resultArray.push(left[leftIndex]);
+						leftIndex++;
+				} else {
+						resultArray.push(right[rightIndex]);
+						rightIndex++;
+				}
+        await timer(100);
+				svg.selectAll('rect').data(data)
+        		.attr('y', d => y(d))
+            .attr('height', d => y(0) - y(d));
+				svg.node();
+		}
+    if (left.length !== 0 || right.length !== 0) {
+        await timer(100);
+				svg.selectAll('rect').data(data)
+        		.attr('y', d => y(d))
+            .attr('height', d => y(0) - y(d));
+				svg.node();
+    		resultArray = resultArray.concat(left.slice(leftIndex));
+    		resultArray = resultArray.concat(right.slice(rightIndex));
+  	}
+		return resultArray;
+}
+
+function mergeSort(data) {
+		if (data.length < 2) {
+				return data;
+		}
+		let mid = Math.floor(data.length / 2);
+		let left = mergeSort(data.slice(0, mid));
+		let right = mergeSort(data.slice(mid));
+    
+    let resultArray = merge(left, right);
+    
+		return resultArray;
+}
+
+// 퀵 정렬
+async function quickSort(data) {
+		if (data.length < 2) {
+				return data;
+		}
+		const pivot = [data[0]];
+		const left = [];
+		const right = [];
+		for (let i = 1; i < data.length; i++) {
+				if (data[i] < pivot) {
+						left.push(data[i]);
+				} else if (data[i] > pivot) {
+						right.push(data[i]);
+				} else {
+						pivot.push(data[i]);
+				}
+		}
+    await timer(100);
+		svg.selectAll('rect').data(data)
+        .attr('y', d => y(d))
+        .attr('height', d => y(0) - y(d));
+		svg.node();
+		return quickSort(left).concat(pivot, quickSort(right));
+}
+
+d3.select("#reset_btn").on("click", function(){
+window.location.reload()
+})
+
+d3.select("#selection_btn").on("click", function() {
+selectionSort(data);
+})
+
+d3.select("#bubble_btn").on("click", function() {
+bubbleSort(data);
+})
+
+d3.select("#insertion_btn").on("click", function() {
+insertionSort(data);
+})
+
+d3.select("#merge_btn").on("click", function() {
+mergeSort(data);
+})
+
+d3.select("#quick_btn").on("click", function() {
+quickSort(data);
+})
+
